fa1bfc3eb48b092ced022995cbe0d43341d2a908
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2012-12-02 16:55:19 -0500

    non blocking model-server

--- model-server/index.js
+++ model-server/index.js
@@ -1,36 +1,39 @@
 var connect = require('connect')
+  , http = require('http')
   , fs = require('fs')
   , path = require('path')
-  , http = require('http')
   , spawn = require('child_process').spawn;
-
-
-
-//server
 
 var app = connect()
   .use(connect.bodyParser())
   .use(function(req, res){
 
-    console.log(req.body);
+    var model = req.body;
 
-    res.end('bye');
+    var pmbuilder = spawn('pmbuilder', ['--input', 'stdin', '-o', 'my_model', '--zip']); //{cwd: xxx}
+    pmbuilder.stdin.write(JSON.stringify(model)+'\n', encoding="utf8");
+    //echo
+    //pmbuilder.stdout.on('data', function (data) {console.log('stdout: ' + data);});
+    //pmbuilder.stderr.on('data', function (data) {console.log('stderr: ' + data);});
+
+    pmbuilder.on('exit', function (code) {
+
+      if(code === 0) {
+        //we want the content-length to use a progress bar so we use stat...
+        fs.stat('my_model.tar.gz', function(err, stats){
+          res.writeHead(200, {'Content-Type': 'application/x-gzip',
+                              'Content-Length': stats.size});
+
+          fs.createReadStream('my_model.tar.gz').pipe(res);
+        });
+
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('\033[91m' + 'FAIL' + '\033[0m' + ': something went wrong\n');
+      }
+
+    });
 
   });
+
 http.createServer(app).listen(3000);
-
-
-//var pmbuilder = spawn('pmbuilder', ['--input', 'stdin', '-o', path.join(odir, 'model'), '--zip'], {cwd: odir});
-//pmbuilder.stdin.write(JSON.stringify(model)+'\n', encoding="utf8");
-//
-//pmbuilder.stdout.on('data', function (data) {console.log('stdout: ' + data);});
-////pmbuilder.stderr.on('data', function (data) {console.log('stderr: ' + data);});
-//
-//pmbuilder.on('exit', function (code) {
-//  console.log('child process exited with code ' + code);
-//
-//  if(code === 0) {
-//    console.log('sending the model');
-//  }
-//
-//});
