315c9d987ae506c79b328d3c4152a4ccc122dec0
sebastien ballesteros <sebastien.ballesteros@gmail.com>
2013-03-04 18:08:51 -0500

    plom build

--- model-server/index.js
+++ model-server/index.js
@@ -49,40 +49,6 @@ app.get('/get/:_id', function(req, res, next){
 
 
 /**
- * install
- **/
-app.post('/install', function(req, res){
-
-  var model = req.body;
-
-  var pmbuilder = spawn('pmbuilder', ['--input', 'stdin', '-o', 'my_model', '--zip']); //{cwd: xxx}
-  pmbuilder.stdin.write(JSON.stringify(model)+'\n', encoding="utf8");
-  //echo
-  //pmbuilder.stdout.on('data', function (data) {console.log('stdout: ' + data);});
-  //pmbuilder.stderr.on('data', function (data) {console.log('stderr: ' + data);});
-
-  pmbuilder.on('exit', function (code) {
-
-    if(code === 0) {
-      //we want the content-length to use a progress bar so we use stat...
-      fs.stat('my_model.tar.gz', function(err, stats){
-        res.set({'Content-Type': 'application/x-gzip',
-                 'Content-Length': stats.size});
-        fs.createReadStream('my_model.tar.gz').pipe(res);
-      });
-
-    } else {
-      res.set('Content-Type', 'text/plain');
-      res.send('\033[91m' + 'FAIL' + '\033[0m' + ': something went wrong\n');
-    }
-
-  });
-
-});
-
-
-
-/**
  * search
  **/
 
