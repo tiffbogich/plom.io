234e2416d3e3d2c1d65e358714e3f9d4eeb2af1b
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2013-04-29 01:24:54 -0400

    display user interests (set of diseases followed)

--- main/routes/user.js
+++ main/routes/user.js
@@ -27,53 +27,43 @@ exports.user = function(req, res, next){
         var q = {
           $or: [
             {from: username},
-            {user_id: username}
+            {username: username}
           ]};
 
+        //events of the followed context
         if(user.context_id && user.context_id.length){
           q['$or']['context_id'] = {$in: user.context_id};
         }
+        //events of the followed users
         if(user.user_id && user.user_id.length){
-          q['$or']['user_id'] = {$in: user.user_id};
+          q['$or']['from'] = {$in: user.user_id};
         }
 
-        e.find(q).sort({_id:-1}).toArray(function(err, docs){
-          if(err) return callback(err);
-          callback(null, docs);
-        });
+        e.find(q).sort({_id:-1}).toArray(callback);
       },
 
       contexts: function(callback){
-        c.find({type:'context', username: req.session.username}, {disease:true, name:true, _id:true}).sort({_id:-1}).toArray(function(err, docs){
-          if(err) return callback(err);
-          callback(null, docs);
-        });
+        c.find({type:'context', username: req.session.username}, {disease:true, name:true, _id:true}).sort({_id:-1}).toArray(callback);
       },
 
       models: function(callback){
-        c.find({type:'link', username: req.session.username}, {context_disease:true, context_name:true, process_name:true, name:true, _id:true}).sort({_id:-1}).toArray(function(err, docs){
-          if(err) return callback(err);
-          callback(null, docs);
-        });
+        c.find({type:'link', username: req.session.username}, {context_disease:true, context_name:true, process_name:true, name:true, _id:true}).sort({_id:-1}).toArray(callback);
       },
 
       thetas: function(callback){
-        c.find({type:'theta', username: req.session.username}, {context_disease:true, context_name:true, process_name:true, link_name:true, name:true, _id:true, link_id:true}).sort({_id:-1}).toArray(function(err, docs){
-          if(err) return callback(err);
-          callback(null, docs);
-        });
+        c.find({type:'theta', username: req.session.username}, {context_disease:true, context_name:true, process_name:true, link_name:true, name:true, _id:true, link_id:true}).sort({_id:-1}).toArray(callback);
       },
 
       reviews: function(callback){
-        r.find({username: req.session.username}).sort({_id:-1}).toArray(function(err, docs){
-          if(err) return callback(err);
-          callback(null, docs);
-        });
-      }
-      
-    },
+        r.find({username: req.session.username}).sort({_id:-1}).toArray(callback);
+      },
 
+      interests: function(callback){
+        c.distinct('disease', {_id: {$in: user.context_id}}, callback);
+      }
+    },
                    function(err, results) {
+                     console.log(results.interests);
                      results.user = user;                     
                      res.render('user', results);                    
                    });
@@ -93,7 +83,7 @@ exports.postFollow = function(req, res, next){
     , key = (req.body.action === 'follow') ? '$addToSet' : '$pull'
     , what = req.params.what;
 
-  update[key] = (what === 'user') ? {user_id: req.body.user_id}: {context_id: req.session.context_id};
+  update[key] = (what === 'user') ? {user_id: req.body.username}: {context_id: new ObjectID(req.session.context_id)};
 
   u.update({_id: req.session.username}, update, {safe:true}, function(err){
     if(err) {
@@ -115,7 +105,7 @@ exports.postFollow = function(req, res, next){
           context: req.session.context_name
         };
       } else {
-        mye.user_id = req.body.user_id;
+        mye.username = req.body.username;
       }
 
       var e = req.app.get('events');
